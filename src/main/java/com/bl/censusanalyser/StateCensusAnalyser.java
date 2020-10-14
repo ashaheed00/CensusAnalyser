ed78a9cd2eb2ad1a2f0cf7f06c5faa876c01024c
ashaheed00 <ashaheed00@gmail.com>
2020-10-14 18:52:30 +0530

    Welcome message in master branch

---
+++ src/main/java/com/bl/censusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,8 @@
+package com.bl.censusanalyser;
+
+public class StateCensusAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Indian State Census Problem");
+	}
+}
