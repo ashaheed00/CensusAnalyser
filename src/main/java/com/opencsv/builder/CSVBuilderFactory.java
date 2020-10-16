8548f04b5ceac49a9d203731c8b7ef838654eac0
ashaheed00 <ashaheed00@gmail.com>
2020-10-16 20:01:48 +0530

    Refactor4B: removed opencsv.build package and created a jar file out of it and imported into the project

--- src/main/java/com/opencsv/builder/CSVBuilderFactory.java
+++
@@ -1,7 +0,0 @@
-package com.opencsv.builder;
-
-public class CSVBuilderFactory {
-	public static <E> ICSVBuilder createCSVBuilder() {
-		return new OpenCsvBuilder<E>();
-	}
-}
--- src/main/java/com/opencsv/builder/ICSVBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.opencsv.builder;
-
-import java.io.Reader;
-import java.util.Iterator;
-
-public interface ICSVBuilder {
-	public <E> Iterator<E> getCsvFileIterator(Reader reader, Class<E> csvCLass) throws CSVException;
-}
